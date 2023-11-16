a0a409aaadd229ed7ac23a4d5ea520d55b0351fd
Bruvv <nvoorn@gmail.com>
2023-11-16 11:48:40 +0100

    clean up

--- waze_map_editor_budy.js
+++ waze_map_editor_budy.js
@@ -11,45 +11,47 @@
 // ==/UserScript==
 
 /* global proj4 */
-proj4.defs("EPSG:28992", "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.237,50.0087,465.658,-0.406857,0.350733,-1.87035,4.0812 +units=m +no_defs");
+proj4.defs(
+	"EPSG:28992",
+	"+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.237,50.0087,465.658,-0.406857,0.350733,-1.87035,4.0812 +units=m +no_defs"
+);
 
-(function() {
-    'use strict';
+(function () {
+	"use strict";
 
-    window.setInterval(function() {
-        let permaElement = document.querySelector('a.permalink');
+	window.setInterval(function () {
+		let permaElement = document.querySelector("a.permalink");
 
-        if(permaElement != null) {
-            let fullURL = document.querySelector('a.permalink').href;
-            let index = fullURL.indexOf('/editor');
-            let path = fullURL.substr(index, fullURL.length);
-            path = path.replace(/&s=[0-9]*/, '');
-            path = path.replace(/&marker=(true|false)*/, '');
-            window.history.replaceState('', '', path);
-        }
-    }, 1000);
+		if (permaElement != null) {
+			let fullURL = document.querySelector("a.permalink").href;
+			let index = fullURL.indexOf("/editor");
+			let path = fullURL.substr(index, fullURL.length);
+			path = path.replace(/&s=[0-9]*/, "");
+			path = path.replace(/&marker=(true|false)*/, "");
+			window.history.replaceState("", "", path);
+		}
+	}, 1000);
 })();
 
-
 function calculateBAGZoom(wazeZoom) {
-    // Map Waze zoom levels to BAG zoom levels
-    return wazeZoom - 4; // Example conversion
+	// Map Waze zoom levels to BAG zoom levels
+	return wazeZoom - 4;
 }
 function calculateMapillaryZoom(wazeZoom) {
-    // Convert Waze zoom level to Mapillary zoom level
-    // Adjust this mapping as needed
-    return wazeZoom - 1; // Example conversion
+	// Convert Waze zoom level to Mapillary zoom level
+	// Adjust this mapping as needed
+	return wazeZoom - 1;
 }
 function calculateSatellietDataPortaalZoom(wazeZoom) {
-    // Convert Waze zoom level to Satelliet Data Portaal zoom level
-    // Adjust this mapping as needed
-    return wazeZoom; // Example conversion
+	// Convert Waze zoom level to Satelliet Data Portaal zoom level
+	// Adjust this mapping as needed
+	return wazeZoom;
 }
 
-(function() {
-    'use strict';
+(function () {
+	"use strict";
 
-    GM_addStyle(`
+	GM_addStyle(`
         .dropdown {
             position: fixed;
             top: 50%;
@@ -90,100 +92,96 @@ function calculateSatellietDataPortaalZoom(wazeZoom) {
         }
     `);
 
-    let dropdown = document.createElement('div');
-    dropdown.className = 'dropdown';
+	let dropdown = document.createElement("div");
+	dropdown.className = "dropdown";
 
-    let logo = document.createElement('img');
-    logo.className = 'dropdown-logo';
-    logo.src = 'https://dronevlieger.nl/wazelogo.png';
-    dropdown.appendChild(logo);
+	let logo = document.createElement("img");
+	logo.className = "dropdown-logo";
+	logo.src = "https://dronevlieger.nl/wazelogo.png";
+	dropdown.appendChild(logo);
 
-    let dropdownContent = document.createElement('div');
-    dropdownContent.className = 'dropdown-content';
+	let dropdownContent = document.createElement("div");
+	dropdownContent.className = "dropdown-content";
 
-    let maps = [
-        { name: 'Google Maps', logo: 'URL_TO_GOOGLE_MAPS_LOGO' },
-        { name: 'Mapillary', logo: 'URL_TO_MAPILLARY_LOGO' },
-        { name: 'Satellietdataportaal', logo: 'URL_TO_SATELLIETDATAPORTAAL_LOGO' },
-        { name: 'Wegstatus', logo: 'URL_TO_WEGSTATUS_LOGO' },
-        { name: 'Melvin', logo: 'URL_TO_MELVIN_LOGO' },
-        { name: 'BAG Viewer', logo: 'URL_TO_BAG_VIEWER_LOGO' }
-        // Add more as needed
-    ];
+	let maps = [
+		{ name: "Google Maps", logo: "URL_TO_GOOGLE_MAPS_LOGO" },
+		{ name: "Mapillary", logo: "URL_TO_MAPILLARY_LOGO" },
+		{ name: "Satellietdataportaal", logo: "URL_TO_SATELLIETDATAPORTAAL_LOGO" },
+		{ name: "Wegstatus", logo: "URL_TO_WEGSTATUS_LOGO" },
+		{ name: "Melvin", logo: "URL_TO_MELVIN_LOGO" },
+		{ name: "BAG Viewer", logo: "URL_TO_BAG_VIEWER_LOGO" },
+	];
 
-    maps.forEach(function(map) {
-        let link = document.createElement('a');
-        link.href = '#';
+	maps.forEach(function (map) {
+		let link = document.createElement("a");
+		link.href = "#";
 
-        let img = document.createElement('img');
-        img.src = map.logo;
-        link.appendChild(img);
+		let img = document.createElement("img");
+		img.src = map.logo;
+		link.appendChild(img);
 
-        let text = document.createTextNode(map.name);
-        link.appendChild(text);
+		let text = document.createTextNode(map.name);
+		link.appendChild(text);
 
-        link.addEventListener('click', function() {
-            openMap(map.name); // Function to handle map opening
-        });
+		link.addEventListener("click", function () {
+			openMap(map.name); // Function to handle map opening
+		});
 
-        dropdownContent.appendChild(link);
-    });
+		dropdownContent.appendChild(link);
+	});
 
-    dropdown.appendChild(dropdownContent);
-    document.body.appendChild(dropdown);
+	dropdown.appendChild(dropdownContent);
+	document.body.appendChild(dropdown);
 
-    function openMap(mapName) {
-        // Extract coordinates from the current URL
-        let params = new URLSearchParams(window.location.search);
-        let lat = parseFloat(params.get('lat'));
-        let lon = parseFloat(params.get('lon'));
-        let wazeZoom = parseInt(params.get('zoomLevel'));
-        let mapillaryZoom = calculateMapillaryZoom(wazeZoom);
-        let satellietZoom = calculateSatellietDataPortaalZoom(wazeZoom);
-        let bagZoom = calculateBAGZoom(wazeZoom);
-        let transformedCoordinates = proj4('EPSG:4326', 'EPSG:28992', [lon, lat]);
+	function openMap(mapName) {
+		// Extract coordinates from the current URL
+		let params = new URLSearchParams(window.location.search);
+		let lat = parseFloat(params.get("lat"));
+		let lon = parseFloat(params.get("lon"));
+		let wazeZoom = parseInt(params.get("zoomLevel"));
+		let mapillaryZoom = calculateMapillaryZoom(wazeZoom);
+		let satellietZoom = calculateSatellietDataPortaalZoom(wazeZoom);
+		let bagZoom = calculateBAGZoom(wazeZoom);
+		let transformedCoordinates = proj4("EPSG:4326", "EPSG:28992", [lon, lat]);
 
-        if (!isFinite(lat) || !isFinite(lon)) {
-            alert('Invalid coordinates!');
-            return;
-        }
+		if (!isFinite(lat) || !isFinite(lon)) {
+			alert("Invalid coordinates!");
+			return;
+		}
 
-        let url = '';
-        switch(mapName) {
-            case 'Google Maps':
-                url = `https://www.google.com/maps/?q=${lat},${lon}`;
-                break;
-            case 'Mapillary':
-                url = `https://www.mapillary.com/app/?lat=${lat}&lng=${lon}&z=${mapillaryZoom}`; // Adjust zoom level as needed
-                break;
-            case 'Satellietdataportaal':
-                // Assuming similar zoom level handling as Mapillary
-                url = `https://www.satellietdataportaal.nl/?base=brtachtergrondkaart&loc=${lat}%2C${lon}%2C${satellietZoom}z&overlay=mos-0`;
-                break;
-            case 'Wegstatus':
-                // Assuming zoom level is not needed or is handled differently
-                url = `https://www.wegstatus.nl/?lat=${lat}&lon=${lon}`;
-                break;
-            case 'Melvin': {
-                const offsetLat = 0.002; // Latitude offset for bounding box
-                const offsetLon = 0.003; // Longitude offset for bounding box
+		let url = "";
+		switch (mapName) {
+			case "Google Maps":
+				url = `https://www.google.com/maps/?q=${lat},${lon}`;
+				break;
+			case "Mapillary":
+				url = `https://www.mapillary.com/app/?lat=${lat}&lng=${lon}&z=${mapillaryZoom}`; // Adjust zoom level as needed
+				break;
+			case "Satellietdataportaal":
+				url = `https://www.satellietdataportaal.nl/?base=brtachtergrondkaart&loc=${lat}%2C${lon}%2C${satellietZoom}z&overlay=mos-0`;
+				break;
+			case "Wegstatus":
+				url = `https://www.wegstatus.nl/?lat=${lat}&lon=${lon}`;
+				break;
+			case "Melvin": {
+				const offsetLat = 0.002; // Latitude offset for bounding box
+				const offsetLon = 0.003; // Longitude offset for bounding box
 
-                let swLat = lat - offsetLat;
-                let swLon = lon - offsetLon;
-                let neLat = lat + offsetLat;
-                let neLon = lon + offsetLon;
+				let swLat = lat - offsetLat;
+				let swLon = lon - offsetLon;
+				let neLat = lat + offsetLat;
+				let neLon = lon + offsetLon;
 
-                url = `https://melvin.ndw.nu/public?sw=${swLat},%20${swLon}&ne=${neLat},%20${neLon}&showHeader=false`;
-                break;
-            }
-            case 'BAG Viewer':
-                url = `https://bagviewer.kadaster.nl/lvbag/bag-viewer/?geometry.x=${transformedCoordinates[0]}&geometry.y=${transformedCoordinates[1]}&zoomlevel=${bagZoom}`;
-                break;
-                // Add more cases for other map services as needed
-        }
+				url = `https://melvin.ndw.nu/public?sw=${swLat},%20${swLon}&ne=${neLat},%20${neLon}&showHeader=false`;
+				break;
+			}
+			case "BAG Viewer":
+				url = `https://bagviewer.kadaster.nl/lvbag/bag-viewer/?geometry.x=${transformedCoordinates[0]}&geometry.y=${transformedCoordinates[1]}&zoomlevel=${bagZoom}`;
+				break;
+		}
 
-        if (url) {
-            window.open(url, '_blank');
-        }
-    }
-    })();
+		if (url) {
+			window.open(url, "_blank");
+		}
+	}
+})();
